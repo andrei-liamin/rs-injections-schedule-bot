0bd15390daca78e19ea51636f4f6d920f7b5dc99
Андрей Лямин <ghappyjohnny@gmail.com>
2020-12-14 23:39:51 +0300

    echo bot

--- bot.py
+++ bot.py
@@ -5,8 +5,8 @@ TOKEN = '1438159737:AAFrC7GeLkJi_wpKVkl9DB46fgVVmO9elQo'
 bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start', 'help'])
-def send_welcome(message):
-	bot.reply_to(message, "Привет")
+def send_welcome(message: Message):
+	bot.reply_to(message, message)
 
 @bot.message_handler(func=lambda message: True)
 def upper(message: Message):
