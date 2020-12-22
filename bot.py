15b5ef3c02ba3b3e3f136b3e026ace4097d44079
Андрей Лямин <ghappyjohnny@gmail.com>
2020-12-23 01:03:04 +0300

    test of local timezone detection

--- bot.py
+++ bot.py
@@ -1,7 +1,9 @@
 import telebot
-import datetime
+from tzlocal import get_localzone
+from datetime import datetime
 from telebot.types import Message
 from apscheduler.schedulers.background import BackgroundScheduler
+
 
 TOKEN = '1438159737:AAFrC7GeLkJi_wpKVkl9DB46fgVVmO9elQo'
 bot = telebot.TeleBot(TOKEN)
@@ -10,7 +12,7 @@ body_scheme_photo_id = 'AgACAgIAAxkBAAOXX95mA4r7tTKG0l6SKkl0JfQTTs0AAnqtMRuanlFK
 # background scheduler
 sched = BackgroundScheduler()
 
-@sched.scheduled_job('cron', day_of_week='mon-sun', hour=12, minute=42, second=0)
+@sched.scheduled_job('cron', day_of_week='mon-sun', hour=1, minute=5, second=0, timezone=get_localzone())
 def timed_job():
   bot.send_photo(116733030, photo=body_scheme_photo_id, caption="Пора ширнуться!")
 
@@ -20,7 +22,15 @@ sched.start()
 
 @bot.message_handler(commands=['start', 'help'])
 def message_props(message: Message):
-	bot.reply_to(message, message)
+	bot.reply_to(message, datetime.now())
+
+@bot.message_handler(commands=['stz'])
+def message_props(message: Message):
+	bot.reply_to(message, datetime.now())
+
+@bot.message_handler(commands=['ltz'])
+def message_props(message: Message):
+	bot.reply_to(message, get_localzone())
 
 @bot.message_handler(content_types=['photo'])
 def photo_props(message: Message):
