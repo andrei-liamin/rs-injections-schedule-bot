431f69216c275d90f00dbdce17b247551e265bfd
Andrew Lyamin <ghappyjohnny@gmail.com>
2024-03-24 19:01:03 +0300

    works only with my id and now.weekday() method fix

--- bot.py
+++ bot.py
@@ -41,12 +41,12 @@ pause_duration = 2 * 60 * 60
 @bot.message_handler(content_types=['photo'])
 def get_photo_id(message):
     file_id = message.photo[-1].file_id
-    bot.send_message(message.chat.id, file_id)
+    bot.send_message(my_chat_id, file_id)
 
 # Обработчик команды /start
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id, "Привет! Я телеграм-бот, который отправляет картинки каждые полчаса с понедельника по пятницу с 11 утра до полуночи. Нажмите кнопку 'Готово', чтобы остановить отправку на текущий день, или 'Пауза на 2 часа', чтобы сделать паузу в отправке.")
+    bot.send_message(my_chat_id, "Привет! Я телеграм-бот, который отправляет картинки каждые полчаса с понедельника по пятницу с 11 утра до полуночи. Нажмите кнопку 'Готово', чтобы остановить отправку на текущий день, или 'Пауза на 2 часа', чтобы сделать паузу в отправке.")
 
 # cleanup buttons
 markup_clear = types.ReplyKeyboardRemove()
@@ -56,32 +56,36 @@ markup_clear = types.ReplyKeyboardRemove()
 def stop_sending(message):
     global is_paused
     is_paused = True
-    bot.send_message(message.chat.id, "Отправка картинок остановлена на текущий день.", reply_markup=markup_clear)
+    bot.send_message(my_chat_id, "Отправка картинок остановлена на текущий день.", reply_markup=markup_clear)
 
 # Обработчик кнопки "Пауза на 2 часа"
-@bot.message_handler(func=lambda message: message.text == "Пауза на 2 часа")
+@bot.message_handler(func=lambda message: message.text == "Напомни через 2 часа")
 def pause_sending(message):
     global is_paused
     is_paused = True
-    bot.send_message(message.chat.id, "Отправка картинок приостановлена на следующие два часа.", reply_markup=markup_clear)
+    bot.send_message(my_chat_id, "Отправка картинок приостановлена на следующие два часа.", reply_markup=markup_clear)
     time.sleep(pause_duration)
     is_paused = False
-    bot.send_message(message.chat.id, "Пауза завершена. Продолжаем отправку картинок.", reply_markup=markup_clear)
+    bot.send_message(my_chat_id, "Пауза завершена. Продолжаем отправку картинок.", reply_markup=markup_clear)
 
 # Функция для отправки картинок
 def send_image():
     while True:
         if not is_paused:
-            week_int = math.floor((datetime.now().date() - date(2024, 4, 2)).days / 7) % 8
-            week = str(week_int + 1)
-            week_day = datetime.now().strftime("%A")
-            caption = week + " " + week_day
-            body_scheme_photo_id = body_scheme_photo_ids[week_int][schedule_days.index(now.weekday)]
             now = datetime.now()
+            week_int = math.floor((now.date() - date(2024, 4, 2)).days / 7) % 8
+            week = str(week_int + 1)
+            week_day = now.strftime("%A")
+            caption = week + " " + week_day
+            body_scheme_photo_id = body_scheme_photo_ids[week_int][schedule_days.index(now.weekday())]
             if now.weekday() in schedule_days and start_hour <= now.hour < end_hour:
                 # Отправляем картинку (замените на свой код)
                 # bot.send_photo(chat_id, photo=open('image.jpg', 'rb'))
-                bot.send_photo(my_chat_id, photo=body_scheme_photo_id, caption=caption, reply_markup=markup)
+                try:
+                    bot.send_photo(my_chat_id, photo=body_scheme_photo_id, caption=caption, reply_markup=markup)
+                except telebot.apihelper.ApiException as e:
+                    print(e)
+                    bot.send_message(my_chat_id, e, reply_markup=markup_clear)
                 pass
         time.sleep(1800)  # Пауза 30 минут
 
